667ecbec5f143fa272075428cc11baa02217d2f8
Yannick <yannick.dorn@gmail.com>
2021-10-05 23:36:45 -0400

    recursion is broken although it was literally working earlier....

--- index.py
+++ index.py
@@ -18,28 +18,59 @@ random.shuffle(names)
 print(names)
 			
 class Player:
-	def __init__(self, name:str, target: object, dead=False, kills=0):
+	def __new__(self, name:str, dead=False, kills=0) -> object:
 		self.name = name
-		self.target = target
+		self.target = None
 		self.dead = dead
 		self.kills = kills
+		return self
+
+	# I don't understand python, so just leave this please for god's sake.
+	def get_player(self) -> object: return self
+
 	def kill(self) -> object:
 		self.dead = True
 		return self.target
 	def score(self) -> int:
 		self.kills = self.kills + 1
 		return self.kills
-	def set_target(self, target:object) -> object:
+	def set_target(self, target:object) -> None:
 		self.target = target
-		return self.target
 	def __str__(self) -> str:
 		return f"{self.name} -> {self.target.name}"
+	def __next__(self) -> object:
+		if self.target is None: return StopIteration
+		return self.target
+	def __str__(self) -> str:
+		return f"{self.name} status: {self.dead} target: {self.target} score: {self.kills}"
+	def __iter__(self) -> str:
+		self.n = 0
+		return self
+
+class PlayerList:
+	def __init__(self, names):
+		self.first = self._player_chain(names)
+	def _player_chain(self, names) -> Player:
+		name = names.pop()
+		print(names)
+		if len(names) == 0:
+			player = Player(name)
+			print(f"last {player.name}")
+			return player
+		player = Player(name)
+		print(player.name)
+		# player.set_target(self._player_chain(names))
+		temp = self._player_chain(names)
+		print(f"{player.name} has {temp.name}")
+		# print(f'{player.name} has {player.target.name}\n')
+		return player
+	def __iter__(self):
+		self.n = 0
+		return self.first
+
+PlayerList(names)
+
+	
 
 
-def generate_persons(names: list) -> Player:
-	first_player:Player = Player(names.pop(), target=None)
-	previous_player: Player = first_player
-	while len(names) > 0:
-		previous_player = previous_player.set_target(names.pop(), target=None)
-	return previous_player.set_target(first_player)
-print(generate_persons(names))
+	
